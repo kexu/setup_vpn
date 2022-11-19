0d3bb941c77a6f2e81f62ede49c3498cc487be54
KEXU <kelvin.k.xu@gmail.com>
2022-11-19 21:39:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 echo "Setup v2ray VPN..."
 wget https://github.com/v2ray/v2ray-core/releases/download/v4.28.2/v2ray-linux-64.zip
+mkdir v2ray-linux-64
+mv v2ray-linux-64.zip ./v2ray-linux-64
+cd v2ray-linux-64
+unzip v2ray-linux-64.zip
+
